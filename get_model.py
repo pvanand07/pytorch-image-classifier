637f4e5d2fa32bc280dd03b03aa4f0969cff4b62
Siva <pvanand07@gmail.com>
2023-03-12 23:17:16 +0530

    refactor: Modify final layers of pre-trained model
    Modify final layers of pre-trained models to create a custom classifier for flower classification task.

--- get_model.py
+++ get_model.py
@@ -42,3 +42,56 @@ def import_model(model_name):
         param.requires_grad = False
     return model
 
+
+##-------------------------------------------------------------------------------------------------#
+##   1.2 LOAD AND CHANGE FINAL LAYERS OF MODEL                                                     #
+##-------------------------------------------------------------------------------------------------#
+# Resnet LOAD
+def Load_model(model_name,hidden_units, learning_rate):
+
+  """
+    Loads the pre-trained model and modifies the final layers to create a custom classifier 
+    for the flower classification task.
+
+    Args:
+    - model_name (str): the name of the pre-trained model to use from ['resnet18', 'efficientnet_v2_l', 'densenet121']
+    - hidden_units (int): the number of neurons in the hidden layer of the custom classifier
+    - learning_rate (float): the learning rate to use for training the model
+    
+    Returns:
+    - model (torch model): the pre-trained model with custom classifier
+    - criterion (torch criterion): the loss function used for training the model
+    - optimizer (torch optimizer): the optimizer used for training the model
+  """
+# Load the pre-trained ResNet18 model
+  model = import_model(model_name);
+  # Set number of input features
+  if model_name == 'efficientnet_v2_l':
+    num_features = 1280
+  elif model_name =='densenet121':
+    num_features = 1024
+  else:
+    num_features = 512 
+# Modify the final layers to create a custom classifier
+  
+  new_classifier = nn.Sequential(
+                    nn.Linear(num_features, hidden_units), # add a linear layer with 512 output neurons
+                    nn.ReLU(), # add a ReLU activation function
+                    nn.Dropout(p=0.2), # add a dropout layer with probability of 0.2
+                    nn.Linear(hidden_units, hidden_units), # add another linear layer with 5120 output neurons
+                    nn.ReLU(), # add a ReLU activation function
+                    nn.Dropout(p=0.2), # add a dropout layer with probability of 0.2
+                    nn.Linear(hidden_units, 102), # add a linear layer with 102 output neurons (number of classes)
+                    nn.LogSoftmax(dim=1) # add a log softmax activation function
+                  )
+  # Replacing the final layer of pretrained model with new_classifier
+  if model_name in (['efficientnet_v2_l','densenet121']):
+    model.classifier = new_classifier 
+    optimizer = optim.Adam(model.classifier.parameters(), lr=0.0003)# use Adam optimizer with a learning rate of 0.0003
+  else:
+    model.fc = new_classifier 
+    optimizer = optim.Adam(model.fc.parameters(), lr=0.0003) 
+
+  # Set the loss function for training the model
+  criterion = nn.NLLLoss() # use negative log likelihood loss as the criterion
+  return model, criterion, optimizer
