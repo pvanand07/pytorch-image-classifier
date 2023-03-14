cca7ebb77a29d4ee770f0ac38b193ca2d461dc57
Siva <pvanand07@gmail.com>
2023-03-14 23:46:36 +0530

    feat: Save model checkpoint
    Save model checkpoint with architecture, hidden units, learning rate, state dictionary, class to index mapping, and optimizer state

--- train.py
+++ train.py
@@ -42,3 +42,20 @@ model = train_model(trainloader, validloader, model, criterion, optimizer, devic
 # Creating save directory if it doesn't exists
 if (arg.save_dir is not None) and (not os.path.exists(arg.save_dir)):
     os.mkdir(arg.save_dir)
+
+# Saving model checkpoint
+checkpoint = {
+"arch":arg.arch,
+"hidden_units" : arg.hidden_units,
+"learning_rate":arg.learning_rate,
+"state_dict": model.state_dict(),
+"class_to_idx": class_to_idx,
+"optimizer_state": optimizer.state_dict
+}
+
+torch.save(
+    checkpoint,
+    arg.save_dir + "/checkpoint.pth" if arg.save_dir is not None else "checkpoint.pth",
+          )
+print("Execution complete. Model saved at {}".format(arg.save_dir + "/checkpoint.pth" if arg.save_dir is not None else "checkpoint.pth"))
+
